e242f8ec655a3904dd6202f38877e0b6accfd275
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-06-10 22:30:04 +0200

    Add app and main components. Fix .babelrc config

---
+++ src/components/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Main} from './main.jsx';
+
+
+export const App = (props) => {
+  return <Main />;
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Yuris revenge!`;
+import React from 'react'
+import ReactDOM from 'react-dom';
+import {App} from './components/app.jsx';
+
+
+const init = () => {
+  ReactDOM.render(
+      <App />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
